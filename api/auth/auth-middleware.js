b12a39bcde7e43d6369eecc08ec356cb9c174e1e
RubesWorld <hi@rubesworld.live>
2021-02-22 17:30:13 -0800

    working on login

---
+++ api/auth/auth-middleware.js
@@ -0,0 +1,39 @@
+const auth = require("./auth-router");
+
+const checkPayload = (req, res, next) => {
+  const { password, username } = req.body;
+  !password || !username
+    ? res.status(401).json("Must have both username and password")
+    : next();
+};
+
+const checkUserIndb = async (req, res, next) => {
+  try {
+    const { username } = req.body;
+    const rows = await User.findBy({ username: username });
+    !rows.length ? next() : res.status(401).json("Username already exists");
+  } catch (e) {
+    res.status(500).json(`Server error: ${e}`);
+  }
+};
+
+const checkUserExists = async (req, res, next) => {
+  try {
+    const { username } = req.body;
+    const rows = await User.findBy({ username: username });
+    if (rows.length) {
+      req.userData = rows[0];
+      next();
+    } else {
+      res.status(401).json("Not inside the db, please check");
+    }
+  } catch (e) {
+    res.status(500).json(`Server error: ${e}`);
+  }
+};
+
+module.exports = {
+  checkPayload,
+  checkUserIndb,
+  checkUserExists,
+};
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -2,10 +2,26 @@ const express = require("express");
 const router = express.Router();
 const User = require("../user/user-model");
 const bcrypt = require("bcryptjs");
+const mw = require("./auth-middleware");
 
-router.post("/register", (req, res) => {
-  console.log("registering");
-});
+router.post(
+  "/register",
+  mw.checkPayload,
+  mw.checkUserIndb,
+  async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      const hash = bcrypt.hashSync(password, 10);
+      const newUser = await User.add({
+        username: username,
+        password: hash,
+      });
+      res.status(201).json(newUser);
+    } catch (e) {
+      res.status(500).json({ message: e.message });
+    }
+  }
+);
 
 router.post("/login", (req, res) => {
   console.log("logging in");
